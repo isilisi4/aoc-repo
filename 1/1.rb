45e9e5367ab82badf2f8b77c785925204d8b3818
isi <ismail.albakri@student.hs-rm.de>
2022-12-03 11:37:43 +0100

    solved it

---
+++ 1/1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby 
+
+biggest_carrying_elf = 0
+count = 0
+File.foreach("input.txt"){ |line|
+    if line == "\n"
+        biggest_carrying_elf = count >= biggest_carrying_elf ? count : biggest_carrying_elf
+        count = 0
+        
+    else
+        count += line.to_i
+    end
+}
+
+puts biggest_carrying_elf
