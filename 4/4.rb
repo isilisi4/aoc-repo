745d546c27db690f3294662e660d878b7dc0ddeb
isi <ismail.albakri@student.hs-rm.de>
2022-12-04 19:40:18 +0100

    optimized solutions

--- 4/4.rb
+++ 4/4.rb
@@ -5,11 +5,9 @@ total = 0
 File.foreach("input.txt"){ |line|
     groups = line.strip.match(/(\d+)-(\d+),(\d+)-(\d+)/)
     elf_section_1, elf_section_2 = ($1.to_i .. $2.to_i).to_a , ($3.to_i .. $4.to_i).to_a
-    if elf_section_1.size > elf_section_2.size
-        total += elf_section_2.&(elf_section_1).eql?(elf_section_2) ? 1 : 0
-    else
-        total += elf_section_1.&(elf_section_2).eql?(elf_section_1) ? 1 : 0
-    end
+    commons = elf_section_2 & elf_section_1
+    
+    total += (commons).eql?(elf_section_2) || (commons).eql?(elf_section_1)  ? 1 : 0
+    
 }
-
 puts total
--- 4/4_part2.rb
+++ 4/4_part2.rb
@@ -5,11 +5,8 @@ total = 0
 File.foreach("input.txt"){ |line|
     groups = line.strip.match(/(\d+)-(\d+),(\d+)-(\d+)/)
     elf_section_1, elf_section_2 = ($1.to_i .. $2.to_i).to_a , ($3.to_i .. $4.to_i).to_a
-    if elf_section_1.size > elf_section_2.size
-        total += elf_section_2.&(elf_section_1).empty? ? 0 : 1
-    else
-        total += elf_section_1.&(elf_section_2).empty? ? 0 : 1
-    end
-}
 
+    total += (elf_section_1 & elf_section_2).empty? ? 0 : 1
+    
+}
 puts total
