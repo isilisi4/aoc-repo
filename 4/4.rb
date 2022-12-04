d0505f5fb6875963fcd680cdcfd3aad11896725d
isi <ismail.albakri@student.hs-rm.de>
2022-12-04 19:26:30 +0100

    replaced intersection with &

--- 4/4.rb
+++ 4/4.rb
@@ -6,9 +6,9 @@ File.foreach("input.txt"){ |line|
     groups = line.strip.match(/(\d+)-(\d+),(\d+)-(\d+)/)
     elf_section_1, elf_section_2 = ($1.to_i .. $2.to_i).to_a , ($3.to_i .. $4.to_i).to_a
     if elf_section_1.size > elf_section_2.size
-        total += elf_section_2.intersection(elf_section_1).eql?(elf_section_2) ? 1 : 0
+        total += elf_section_2.&(elf_section_1).eql?(elf_section_2) ? 1 : 0
     else
-        total += elf_section_1.intersection(elf_section_2).eql?(elf_section_1) ? 1 : 0
+        total += elf_section_1.&(elf_section_2).eql?(elf_section_1) ? 1 : 0
     end
 }
 
