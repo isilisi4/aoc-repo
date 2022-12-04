8a1617830b778505831a3a695b1fc67144f5e761
isi <ismail.albakri@student.hs-rm.de>
2022-12-04 19:44:34 +0100

    removed unused var

--- 4/4_part2.rb
+++ 4/4_part2.rb
@@ -3,7 +3,7 @@
 total = 0
 
 File.foreach("input.txt"){ |line|
-    groups = line.strip.match(/(\d+)-(\d+),(\d+)-(\d+)/)
+    line.strip.match(/(\d+)-(\d+),(\d+)-(\d+)/)
     elf_section_1, elf_section_2 = ($1.to_i .. $2.to_i).to_a , ($3.to_i .. $4.to_i).to_a
 
     total += (elf_section_1 & elf_section_2).empty? ? 0 : 1
